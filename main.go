cd52fac28f969be43d46800b6c6b5f77ee025958
vishu42 <tewatiavishal3@gmail.com>
2023-07-27 22:59:32 +0530

    use temp dir

--- main.go
+++ main.go
@@ -2,25 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/vishu42/gitscan/pkg"
 )
 
 func main() {
 	// get command line arguments
-	// os.Args[0] is the name of the program
 	repo := os.Args[1]
 
-	workdir := "/Users/vishaltewatia/github/vishu42/gitscan/temp/Devops-Node"
+	// create a temp directory
+	workdir := "/tmp/gitscan-" + strconv.Itoa(rand.Intn(1000000))
 
-	// clone repo
-	err := pkg.CloneRepo(repo, "/Users/vishaltewatia/github/vishu42/gitscan/temp/")
+	// get the name of the repo
+	repoSplited := strings.Split(repo, "/")
+	repoName := repoSplited[len(repoSplited)-1]
+	clonedir := workdir + "/" + repoName
+
+	log.Println("Workdir:", workdir)
+
+	// create temp workdir
+	err := pkg.MkDir(workdir)
 	if err != nil {
 		panic(err)
 	}
 
-	c, err := pkg.GetAllCommits(workdir)
+	// remove temp workdir after function execution is done
+	defer func() {
+		err = pkg.RmDir(workdir)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// clone repo
+	err = pkg.CloneRepo(repo, workdir)
+	if err != nil {
+		panic(err)
+	}
+
+	c, err := pkg.GetAllCommits(clonedir)
 	if err != nil {
 		panic(err)
 	}
@@ -29,14 +54,14 @@ func main() {
 
 	for _, commit := range c {
 		// for each commit, list all files
-		files, err := pkg.GetCommitFiles(workdir, commit)
+		files, err := pkg.GetCommitFiles(clonedir, commit)
 		if err != nil {
 			panic(err)
 		}
 
 		// for each file, get file contents
 		for _, file := range files {
-			cr, err := pkg.GetFileContent(workdir, commit, file)
+			cr, err := pkg.GetFileContent(clonedir, commit, file)
 			if err != nil {
 				panic(err)
 			}
--- pkg/utils.go
+++ pkg/utils.go
@@ -3,6 +3,8 @@ package pkg
 import (
 	"fmt"
 	"os/exec"
+
+	"github.com/go-cmd/cmd"
 )
 
 // BinaryExists throws an error if the binary does not exist
@@ -19,4 +21,43 @@ func BinaryExists(binary string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+// HandleStatus returns an error if the command failed to execute or there is a go error in status object
+func HandleStatus(s cmd.Status) error {
+	switch {
+	case s.Error != nil:
+		return s.Error
+	case s.Exit != 0:
+		err := fmt.Errorf("error while running %s\n%q", s.Cmd, s.Stderr)
+		return err
+	default:
+		for _, line := range s.Stdout {
+			fmt.Println(line)
+		}
+	}
+
+	return nil
+}
+
+// RmDir removes a directory
+func RmDir(dir string) (err error) {
+	rmdir := cmd.NewCmd("rm", "-rf", dir)
+	status := <-rmdir.Start()
+	err = HandleStatus(status)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// MkDir creates a directory
+func MkDir(dir string) (err error) {
+	mkdir := cmd.NewCmd("mkdir", dir)
+	status := <-mkdir.Start()
+	err = HandleStatus(status)
+	if err != nil {
+		return err
+	}
+	return nil
 }
